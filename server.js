379946611f775c6f8f52b9b2077cd04bc4c8b9de
Simon Fugere <smonyk@gmail.com>
2018-11-21 15:49:50 -0500

    feature/puckjs-font-control janky font control with puckjs

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 const app = express();
 
 app.use(express.static('public'));
+
+var options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+};
+
+https.createServer(options, app).listen(443);
 
 const server = app.listen(process.env.PORT || 4000, () => {
   console.log(`Listening on port ${ server.address().port }`);
--- themes/variable-text/source/js/main.js
+++ themes/variable-text/source/js/main.js
@@ -32,4 +32,55 @@
     axisSlidersToSamples('#' + li.id + ' input', '#' + li.id + ' .sample');
     li.className += ' loaded';
   });
+
+  const puckBtn = document.getElementById('my-button');
+  const gradeFont = $('#grade .sample');
+
+  // // Called when we get a line of data - updates the light color
+  // function onLine(v) {
+  //   console.log("Received: "+JSON.stringify(v));
+  // }
+  var connection;
+
+  puckBtn.addEventListener("click", function() {
+    if (connection) {
+      connection.close();
+      connection = undefined;
+    }
+    Puck.connect(function(c) {
+      if (!c) {
+        alert("Couldn't connect!");
+        return;
+      }
+      connection = c;
+
+      connection.on("data", function(d) {
+        let fontWeight = Math.round(((Math.abs(d) - 30) / 55) * 70 + 80);
+        let fontWidth = Math.round(((Math.abs(d) - 30) / 55) * 30 + 90);
+
+        if (!isNaN(fontWeight) || !isNaN(fontWidth)) {
+          gradeFont.css('font-variation-settings', '"wght" ' + fontWeight + ', "wdth" ' + fontWidth);
+          console.log('font weight: ' + fontWeight, 'font width ' + fontWidth);
+        }
+
+      });
+      // First, reset Puck.js
+      connection.write("reset();\n", function() {
+        // Wait for it to reset itself
+        setTimeout(function() {
+          // Now tell it to write data on the current light level to Bluetooth
+          // 10 times a second. Also ensure that when disconnected, Puck.js
+          // resets so the setInterval doesn't keep draining battery.
+          connection.write(`
+            setInterval(function(){
+              NRF.setRSSIHandler(function(rssi){
+                Bluetooth.println(rssi);
+              },1500);
+            });\n`,
+          function() { console.log("Ready..."); });
+        }, 1500);
+      });
+    });
+  });
+
 })();
